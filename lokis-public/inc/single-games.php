b3c0a313dece3634b2b8782942272d70a759d400
anjila0 <anjila@codepixelzmedia.com.np>
2023-05-01 10:52:48 +0545

    updated code in single-games.php

--- lokis-public/inc/single-games.php
+++ lokis-public/inc/single-games.php
@@ -1,26 +1,31 @@
 <?php
 get_header();
 ?>
-<div class="paragraph-content">
-    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, aliquam. Modi a totam, enim eum asperiores,
-        iste esse, at odit labore corrupti blanditiis consectetur autem mollitia debitis officia sit architecto!
-    </P>
+
+<div class="container">
+    <div class="lokis-paragraph-content">
+        <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, aliquam. Modi a totam, enim eum
+            asperiores,
+            iste esse, at odit labore corrupti blanditiis consectetur autem mollitia debitis officia sit architecto!
+        </P>
+    </div>
+    <div class="lokis-iframe">
+        <iframe
+            src="https://games.gdevelop-app.com/game-e80a4ac5-b828-4a36-8e0f-73f85fad831d/index.html?userSlug=antonretro&amp;gameSlug=paintra"
+            allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; keyboard-map *; focus-without-user-activation *; screen-wake-lock; clipboard-read; clipboard-write"
+            sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts allow-same-origin allow-popups-to-escape-sandbox allow-downloads"
+            scrolling="no" allowfullscreen="" style="width:100%;height:100%">
+        </iframe>
+    </div>
+    <div class="lokis-post-form">
+        <form action="" method="post">
+            <label for="answer">Answer:</label>
+            <input type="text" id="answer" name="answer"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
 </div>
-<div class="iframe">
-    <iframe
-        src="https://games.gdevelop-app.com/game-e80a4ac5-b828-4a36-8e0f-73f85fad831d/index.html?userSlug=antonretro&amp;gameSlug=paintra"
-        allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; keyboard-map *; focus-without-user-activation *; screen-wake-lock; clipboard-read; clipboard-write"
-        sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts allow-same-origin allow-popups-to-escape-sandbox allow-downloads"
-        scrolling="no" allowfullscreen="" style="width:100%;height:100%">
-    </iframe>
-</div>
-<div class="post-form">
-    <form action="/action_page.php" method="post" target="_blank">
-        <label for="answer">Answer:</label>
-        <input type="text" id="answer" name="answer"><br><br>
-        <input type="submit" value="Submit">
-    </form>
-</div>
+
 <?php
 get_footer();
 ?>
