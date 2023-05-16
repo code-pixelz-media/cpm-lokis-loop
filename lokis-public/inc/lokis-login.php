8d8ea61bfd17584987fa0befa97d482efb93a612
anjila0 <anjila@codepixelzmedia.com.np>
2023-05-16 11:30:42 +0545

    Lokisloop Login Page
    Created login page for lokisloop dashboard.

--- lokis-public/inc/lokis-login.php
+++ lokis-public/inc/lokis-login.php
@@ -5,10 +5,25 @@ function lokis_loop_login()
 {
     ob_start();
     ?>
+    <div class="lokisloop-loginform-container">
+        <h2>Login Form</h2>
+        <form action="" method="POST">
+            <label for="username" class="form-label">Username:</label>
+            <input type="text" class="form-control" id="username" name="username" />
+            <label for="password" class="form-label">Password:</label><br />
+            <input type="password" class="form-control" id="password" name="password" />
+            <div class="lokisloop-login-checkbox"><input type="checkbox" class="checkbox" id="remember" name="remember" />
+                <label for="remember" class="remember">Remember me</label>
+            </div>
+
+            <input type="submit" class="lokisloop-login-btn" value="Login" />
+        </form>
+        <p>Not a member? <a href="#">Sign up Now</a></p>
+        <p><a href="#">Forgot password?</a></p>
+    </div>
 
 
-
- <?php
+    <?php
     return ob_get_clean();
 }
 add_shortcode('lokis_loop_login', 'lokis_loop_login');
