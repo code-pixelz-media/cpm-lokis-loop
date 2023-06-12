d27f7c70e1cc75052d3e63ed9b064e099f631122
Suman <sumanwagle7000@gmail.com>
2023-06-12 11:05:56 +0545

    user dashboard translation

--- lokis-public/inc/dashboard/lokis-user-dashboard.php
+++ lokis-public/inc/dashboard/lokis-user-dashboard.php
@@ -12,9 +12,6 @@ function lokis_loop_user_dashboard()
 
         <div class="lokisloop-dashboard-container">
             <aside>
-                <div class="lokis-logo">
-                    <a id="home-page" class="nav-link " href="/">Loki's Loop</a>
-                </div>
                 <?php lokis_account_menu(); ?>
             </aside>
             <!-- for user profile -->
@@ -24,7 +21,9 @@ function lokis_loop_user_dashboard()
                     <div class="container">
                         <div class="rightbox">
                             <div class="profile">
-                                <h1>Personal Info</h1>
+                                <h1>
+                                    <?php _e('Personal Info', 'lokis-loop'); ?>
+                                </h1>
                                 <div class="lokis-gamehost-profileform-container">
                                     <div class="lokisloop-profile-form">
                                         <form action="" method="post">
@@ -32,7 +31,9 @@ function lokis_loop_user_dashboard()
                                             <?php wp_nonce_field(-1, 'loki_profile_nonce'); ?>
 
                                             <div class="lokis-form-item">
-                                                <label for="loki-name" class="form_control">Name :</label>
+                                                <label for="loki-name" class="form_control">
+                                                    <?php _e('Name:', 'lokis-loop'); ?>
+                                                </label>
                                                 <p aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-name'><?php echo get_user_meta($user_id, 'loki_fullname', true); ?>
 
@@ -40,7 +41,9 @@ function lokis_loop_user_dashboard()
                                             </div>
 
                                             <div class="lokis-form-item">
-                                                <label for="loki-email" class="form_control"> Email : </label>
+                                                <label for="loki-email" class="form_control">
+                                                    <?php _e('Email:', 'lokis-loop'); ?>
+                                                </label>
                                                 <p aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-email'><?php echo $user_email; ?>
 
@@ -48,7 +51,9 @@ function lokis_loop_user_dashboard()
                                             </div>
 
                                             <div class="lokis-form-item">
-                                                <label for="loki-organization" class="form_control">Organization Name :</label>
+                                                <label for="loki-organization" class="form_control">
+                                                    <?php _e('Organization Name:', 'lokis-loop'); ?>
+                                                </label>
                                                 <input aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-organization'
                                                     value="<?php echo get_user_meta($user_id, 'loki_organization', true); ?>"
@@ -58,63 +63,69 @@ function lokis_loop_user_dashboard()
                                             <?php
                                             $preselected_value = get_user_meta($user_id, 'loki_organization_type', true);
                                             echo '<div class="lokis-form-item lokis-radio">
-                                                    <label for="organizational_name" class="form_control"> What best describes your organization: </label>
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="library" name="loki_organization_type" value="Public-Library"' . ($preselected_value === 'Public-Library' ? ' checked' : '') . '>
-                                                        <label for="library" class="form_control"><span>Public library</span></label>
-                                                    </div>
+                                                <label for="organizational_name" class="form_control">' . __('What best describes your organization:', 'lokis-loop') . '</label>
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="library" name="loki_organization_type" value="Public-Library"' . ($preselected_value === 'Public-Library' ? ' checked' : '') . '>
+                                                    <label for="library" class="form_control"><span>' . __('Public library', 'lokis-loop') . '</span></label>
+                                                </div>
 
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="university/college" name="loki_organization_type" value="University/College"' . ($preselected_value === 'University/College' ? ' checked' : '') . '>
-                                                        <label for="university/college" class="form_control"><span>University/College</span></label>
-                                                    </div>
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="university/college" name="loki_organization_type" value="University/College"' . ($preselected_value === 'University/College' ? ' checked' : '') . '>
+                                                    <label for="university/college" class="form_control"><span>' . __('University/College', 'lokis-loop') . '</span></label>
+                                                </div>
 
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="school" name="loki_organization_type" value="K-12 School"' . ($preselected_value === 'K-12 School' ? ' checked' : '') . '>
-                                                        <label for="school" class="form_control">K-12 school</label>
-                                                    </div>
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="school" name="loki_organization_type" value="K-12 School"' . ($preselected_value === 'K-12 School' ? ' checked' : '') . '>
+                                                    <label for="school" class="form_control">' . __('K-12 school', 'lokis-loop') . '</label>
+                                                </div>
 
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="museum" name="loki_organization_type" value="Museum"' . ($preselected_value === 'Museum' ? ' checked' : '') . '>
-                                                        <label for="museum" class="form_control">Museum</label>
-                                                    </div>
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="museum" name="loki_organization_type" value="Museum"' . ($preselected_value === 'Museum' ? ' checked' : '') . '>
+                                                    <label for="museum" class="form_control">' . __('Museum', 'lokis-loop') . '</label>
+                                                </div>
 
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="non-profit" name="loki_organization_type" value="Non-profit"' . ($preselected_value === 'Non-profit' ? ' checked' : '') . '>
-                                                        <label for="non-profit" class="form_control">Non-profit</label>
-                                                    </div>
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="non-profit" name="loki_organization_type" value="Non-profit"' . ($preselected_value === 'Non-profit' ? ' checked' : '') . '>
+                                                    <label for="non-profit" class="form_control">' . __('Non-profit', 'lokis-loop') . '</label>
+                                                </div>
 
-                                                    <div class="lokisloop-selective-box">
-                                                        <input type="radio" id="other-organization" name="loki_organization_type" value="Other Organization"' . ($preselected_value === 'Other Organization' ? ' checked' : '') . '>
-                                                        <label for="other-organization" class="form_control">Other Organization (non-library)</label>
-                                                    </div>
-                                                </div>';
+                                                <div class="lokisloop-selective-box">
+                                                    <input type="radio" id="other-organization" name="loki_organization_type" value="Other Organization"' . ($preselected_value === 'Other Organization' ? ' checked' : '') . '>
+                                                    <label for="other-organization" class="form_control">' . __('Other Organization (non-library)', 'lokis-loop') . '</label>
+                                                </div>
+                                            </div>';
                                             ?>
 
                                             <div class="lokis-form-item">
-                                                <label for="loki-password" class="form_control">Current Password :</label>
+                                                <label for="loki-password" class="form_control">
+                                                    <?php _e('Current Password:', 'lokis-loop'); ?>
+                                                </label>
                                                 <input aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-old-password' placeholder="Enter your Current Password">
                                             </div>
 
                                             <div class="lokis-form-item">
-                                                <label for="password" class="form_control"> New Password : </label>
+                                                <label for="password" class="form_control">
+                                                    <?php _e('New Password :', 'lokis-loop') ?>
+                                                </label>
                                                 <input aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-new-password' placeholder="Enter your New Password">
                                             </div>
 
                                             <div class="lokis-form-item">
-                                                <label for="password" class="form_control">Re-type Password : </label>
+                                                <label for="password" class="form_control">
+                                                    <?php _e('Re-type Password:', 'lokis-loop'); ?>
+                                                </label>
                                                 <input aria label="single line text" maxlength="4000" class="lokis-form-control"
                                                     id='loki-new-password-retype' placeholder="Re-Type your Password">
                                             </div>
 
-                                            <button type="button" class="edit-button" id="generate-password">Generate
-                                                Password
+                                            <button type="button" class="edit-button" id="generate-password">
+                                                <?php _e('Generate Password', 'lokis-loop'); ?>
                                             </button>
 
                                             <button class="edit-button" id='lokis-profile-update-button' type="submit">
-                                                Update
+                                                <?php _e('Update', 'lokis-loop'); ?>
                                             </button>
                                         </form>
                                         <div id='lokis-error-message'></div>
